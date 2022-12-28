9569b41cb3f4559acce906c0e7d7cc0899241005
Jakin-Kim <kimyagin94@gmail.com>
2022-12-28 23:18:35 +0900

    chore: components have been initialized

---
+++ src/components/Contents/Activity.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Activity() {
+  return (
+    <div>Activity</div>
+  )
+}
+
+export default Activity
---
+++ src/components/Contents/Studies.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Studies() {
+  return (
+    <div>Studies</div>
+  )
+}
+
+export default Studies
