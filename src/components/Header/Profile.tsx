066ccfd02392776055c293621a947ff1143d9020
Jakin-Kim <kimyagin94@gmail.com>
2023-01-18 14:34:13 +0900

    css: 'user-select' property on img is added for removing an auto-focused blue box

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -23,10 +23,7 @@ const Styled = {
         img {
             width: 75%;
             height: 75%;
-            -webkit-user-select : none; // 파란박스 자동생성 제거(safari, chrome)
-            -moz-user-select : none; // 파란박스 자동생성 제거(mozilla)
-            -ms-user-select : none; // 파란박스 자동생성 제거(konqueror)
-            -khtml-user-select : none; // 파란박스 자동생성 제거(IE10+)
+            user-select : none; // 파란박스 자동생성 제거
             
             @media (max-width: 650px) {
                 width: 55%;
