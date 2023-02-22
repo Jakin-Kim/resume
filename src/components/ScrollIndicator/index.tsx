64e3c2ff94e4f2e8b741fa7aaadca494eee0f103
Jakin-Kim <kimyagin94@gmail.com>
2023-02-22 23:10:30 +0900

    feat: ScrollIndicator undisplayed issue has been resolved with 'z-index' revise

--- src/components/ScrollIndicator/index.tsx
+++ src/components/ScrollIndicator/index.tsx
@@ -30,7 +30,7 @@ const Styled = {
     background-color: black;
     height: 5px;
     position: fixed;
-    z-index: 1;
+    z-index: 2;
     width: 100%;
     top: 0;
     left: 0;
---
+++ src/styles/theme.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const mode = {
+    dark:`
+        background-color: black;
+        color: white;
+    `,
+};
+
+const theme = {
+    mode,
+};
+
+export default theme;
